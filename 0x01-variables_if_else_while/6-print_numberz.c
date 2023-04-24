2e6e6819509b32d144a9c77198ecea698d0f30a4
be-dotcom <selinahkavili01@gmail.com>
2023-04-24 04:37:30 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
@@ -9,7 +10,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 0 ; n < 10 ; n++)
+	for (n = '0' ; n <= '9' ; n++)
 	{
 		putchar(n);
 	}
