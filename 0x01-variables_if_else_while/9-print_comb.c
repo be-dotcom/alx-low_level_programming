23b7bbaa77b6837110f15f0b172f8fe2cae11610
be-dotcom <selinahkavili01@gmail.com>
2023-04-24 07:15:12 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,17 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
-  * main - prints all possible combinations of single-digit numbers
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j, k;
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i);
-	}
-	for (j = 0; j < 10; j++)
-	{
-		putchar(j);
-	}
-	for (k = 0; k < 10; k++)
-	{
-		putchar(k);
-	}
-	putchar('\n');
-	return (0);
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
