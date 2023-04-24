b20f34102f1b9c6719695ef4b3ba8b8b2acf6a69
be-dotcom <selinahkavili01@gmail.com>
2023-04-24 04:14:06 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,31 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - prints the alphabet in lowercase,
- main - prints the alphabet in uppercase,
- * followed by a new line
- * Return: Always 0 (Success)
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
-
 int main(void)
 {
-char ch;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-ch = 'a';
-{
-char ch = 'a';
+	char ch = 'a';
 
-while (ch <= 'z')
-{
-if (ch != e && ch != q)
-{
-putchar(ch);
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
-ch++;
+
+
 }
-putchar('\n');
-return (0);
-}
+
 }
