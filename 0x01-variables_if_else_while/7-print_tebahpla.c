efb442923e9ec03eac5b5a2469bb601971c5cb7b
be-dotcom <selinahkavili01@gmail.com>
2023-04-24 04:49:51 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - prints the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 	char ch;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	for (ch = z ; ch <= a ; ch--)
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
 		putchar(ch);
-	putchar("\n");
+	}
+	putchar('\n');
 	return (0);
 }
