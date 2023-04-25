e2808d8379e4c83a76162d02feb84e3ad2b4e308
be-dotcom <selinahkavili01@gmail.com>
2023-04-25 03:26:18 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
-	#include "main.h"
-	/**
-	* main - Entry point
-	* Description: prints "_putchar" , followed by a new line
-	* Return: Always 0 (Success)
-	*/
+#include "main.h"
+/**
+* main - Entry point
+* Description: prints "_putchar" , followed by a new line
+* Return: Always 0 (Success)
+*/
 
-	int main(void)
-	{
-	/* your code goes there */
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
